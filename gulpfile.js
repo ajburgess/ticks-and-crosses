0a66aecc6f9259285141cfb2937f58b860a9f23c
Andrew Burgess <andrewjohnburgess@outlook.com>
2021-02-14 15:26:17 +0000

    Improvements suggested by Yaroslav

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,59 @@
-// NB: This file is no loger needed.
-// I've switched from trying to "cache-bust" the CSS and JS files
-// to just making express deliver the static files with no caching
-// - so we can update these files and have the new versions flow
-// to user's browsers OK.
+// // NB: This file is no loger needed.
+// // I've switched from trying to "cache-bust" the CSS and JS files
+// // to just making express deliver the static files with no caching
+// // - so we can update these files and have the new versions flow
+// // to user's browsers OK.
 
-var gulp = require('gulp');
-var del = require('del');
-var sourcemaps = require('gulp-sourcemaps');
-var debug = require('gulp-debug');
-var CacheBuster = require('gulp-cachebust');
-const { dest } = require('gulp');
-var cachebust = new CacheBuster();
+// var gulp = require('gulp');
+// var del = require('del');
+// var sourcemaps = require('gulp-sourcemaps');
+// var debug = require('gulp-debug');
+// var CacheBuster = require('gulp-cachebust');
+// const { dest } = require('gulp');
+// var cachebust = new CacheBuster();
 
-// Cleans the dist output
+// // Cleans the dist output
 
-gulp.task('clean-public', async function () {
-    await del([
-        'dist/public'
-    ]);
-});
+// gulp.task('clean-public', async function () {
+//     await del([
+//         'dist/public'
+//     ]);
+// });
 
-// Prepare cache-busting for the resources (css and js files)
+// // Prepare cache-busting for the resources (css and js files)
 
-// Use this order: 1) other assets, 2) CSS files 3) HTML files 4) JS files
-gulp.task('cache-bust-and-rewrite-public', function() {
-    return gulp.src(['src/public/**/*/!(*.html|*.css|*.js)', 'src/public/**/*/*.css', 'src/public/**/*/*.html', 'src/public/**/*.js'])
-        .pipe(cachebust.resources())
-        .pipe(cachebust.references())
-        .pipe(dest('dist/public'));
-});
+// // Use this order: 1) other assets, 2) CSS files 3) HTML files 4) JS files
+// gulp.task('cache-bust-and-rewrite-public', function() {
+//     return gulp.src(['src/public/**/*/!(*.html|*.css|*.js)', 'src/public/**/*/*.css', 'src/public/**/*/*.html', 'src/public/**/*.js'])
+//         .pipe(cachebust.resources())
+//         .pipe(cachebust.references())
+//         .pipe(dest('dist/public'));
+// });
 
-gulp.task('rewrite-index', function() {
-    return gulp.src('src/public/index.html')
-        .pipe(cachebust.references())
-        .pipe(gulp.dest('dist/public'));
-});
+// gulp.task('rewrite-index', function() {
+//     return gulp.src('src/public/index.html')
+//         .pipe(cachebust.references())
+//         .pipe(gulp.dest('dist/public'));
+// });
 
-// Copy node application across
+// // Copy node application across
 
-gulp.task('copy-nodejs', function() {
-    return gulp.src(['src/*.js', 'package.json'])
-    .pipe(gulp.dest('dist'));
-});
+// gulp.task('copy-nodejs', function() {
+//     return gulp.src(['src/*.js', 'package.json'])
+//     .pipe(gulp.dest('dist'));
+// });
 
-// Rebuild of public folder
+// // Rebuild of public folder
 
-gulp.task('public-build', gulp.series('clean-public', 'cache-bust-and-rewrite-public', 'rewrite-index'));
+// gulp.task('public-build', gulp.series('clean-public', 'cache-bust-and-rewrite-public', 'rewrite-index'));
 
-// Full build
+// // Full build
 
-gulp.task('build', gulp.series('public-build', 'copy-nodejs'));
+// gulp.task('build', gulp.series('public-build', 'copy-nodejs'));
 
-// Watch files in the src directory: if they change, rebuild the dist folder
+// // Watch files in the src directory: if they change, rebuild the dist folder
 
-gulp.task('watch', function() {
-    gulp.watch('src/*.js', gulp.series('copy-nodejs'));
-    gulp.watch('src/public/**/*', gulp.series('public-build'));
-});
+// gulp.task('watch', function() {
+//     gulp.watch('src/*.js', gulp.series('copy-nodejs'));
+//     gulp.watch('src/public/**/*', gulp.series('public-build'));
+// });
